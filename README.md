2978549f6e234f42f768e074267f120362e3c063
Simon Schmoll <simon.schmoll@protonmail.com>
2020-01-07 18:20:25 +0100

    Modify readme and extend install script

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # neovim-config
-This is my Neovim config, hopefully additionally providing a script for reinstalling setup
+This is my Neovim config. The install script is currently not tested, so please make sure everything succeeds when running the script.
 
 Most of the config files are adapted from [here](https://lucasfcosta.com/2019/02/10/terminal-guide-2019.html)
 
 Most of the plugins are also from the awesome talk [here](https://www.youtube.com/watch?v=wlR5gYd6um0).
+
+# Installation
+## Clone this repo
+## Make install script executable, e.g. chmod 770 install.sh
+## Run Install.sh script
+
+# Useful links
+[Generate ssh key for github and add to keychain](https://help.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent)
--- install.sh
+++ install.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 
-if [ -x $(command -v brew) ]; then
-	echo "You first need to install brew"
-	exit 1
+echo "############# Installing brew, vim plug and tpm #################"
+
+if ! type "brew" &> /dev/null; then
+	echo "Installing homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+if ! type "git" &> /dev/null; then
+     echo "Please install git, either through oh my zsh or brew"
+     exit 1
+fi
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "############# Copying dotfiles #################"
+mkdir -p $HOME/.config/nvim && cp dotfiles/init.vim $HOME/.config/nvim/
+cp dotfiles/.zshrc dotfiles/.tmux.conf dotfiles/.gitconfig $HOME/
 
 echo "############# Checking which components to install #################"
 
-if [ -x $(command -v nvim) ]; then
+if ! type "nvim" &> /dev/null; then
 	echo "############# Installing neovim #################"
 	brew install neovim
+    nvim +PlugInstall +qall &> /dev/null
 fi
 
-if [ -x $(command -v nvim) ]; then
-	echo "############# Installing neovim #################"
-	brew install neovim
-fi
-
-if [ -x $(command -v tmux) ]; then
+if ! type "tmux" &> /dev/null; then
 	echo "############# Installing tmux #################"
 	brew install tmux
 fi
 
-if [ -x $(command -v fzf) ]; then
+if ! type "tmuxinator" &> /dev/null; then
+	echo "############# Installing tmuxinator #################"
+	brew install tmuxinator
+fi
+
+if ! type "fzf" &> /dev/null; then
 	echo "############# Installing fzf #################"
 	brew install fzf
 fi
 
-if [ -x $(command -v fzf) ]; then
-	echo "############# Installing fzf #################"
-	brew install fzf
+if ! type "z" &> /dev/null; then
+	echo "############# Installing z #################"
+	brew install z
 fi
 
 
